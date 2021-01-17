aa3d16fa37a014c9997e780d92615629f29d8c3f
Konstantin Borisov <76878696+KonstantinBorisov13@users.noreply.github.com>
2021-01-17 10:43:07 +0200

    Add files via upload

---
+++ Programming Fundamentals with JavaScript/22.Exam Preparation/01.js
@@ -0,0 +1,57 @@
+function activate(input) {
+    let actions = {
+     Contains(rawKey, pattern) {
+        if (rawKey.includes(pattern)) {
+            console.log((`${rawKey} contains ${pattern}.`));
+
+        } else {
+            console.log(`Substring not found!`);   
+        }
+        return rawKey;
+    },
+     Flip(rawKey, mode, start, end) {
+        [start, end] = [Number(start), Number(end)];
+        let first = rawKey.substring(0, start);
+        let second = rawKey.substring(start, end);
+        let third = rawKey.substring(end);
+        if (mode == "Upper") {
+            second = second.toLocaleUpperCase();
+        } else {
+            second = second.toLocaleLowerCase();
+        }
+
+        let result = first + second + third;
+        console.log(result);
+        return result;
+    },
+     Slice(rawKey, start, end) {
+        [start, end] = [Number(start), Number(end)];
+        let first = rawKey.substring(0, start);
+        let second = rawKey.substring(start, end);
+        let third = rawKey.substring(end);
+        let result = first + third;
+        console.log(result);
+        return result;
+    }
+};
+
+    let rawKey = input.shift();
+    let line;
+    while ((line = input.shift()) != "Generate") {
+        let [actionName, ...params] = line.split('>>>');
+
+        rawKey = actions[actionName](rawKey, ...params);
+
+    }
+    console.log(`Your activation key is: ${rawKey}`);
+}
+
+activate([
+    'abcdefghijklmnopqrstuvwxyz',
+    'Slice>>>2>>>6',
+    'Flip>>>Upper>>>3>>>14',
+    'Flip>>>Lower>>>5>>>7',
+    'Contains>>>def',
+    'Contains>>>deF',
+    'Generate'
+])
